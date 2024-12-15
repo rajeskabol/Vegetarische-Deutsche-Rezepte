9a5cea5150771eb82281421207a8e5b5f39697b5
rajeskabol <rajeskabol@gmail.com>
2024-12-16 05:31:15 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  title: "JUDUL",
-  tagline: "Coloring Me Free Coloring Page",
-  description: "Coloring Me Free Coloring Pag.",
-  keywords: "coloring,coloringkids,agc,cdom blog",
-  author: "Coloringme.biz.id",
-  favicon: "https://pugjs.org/images/favicon.ico",
+  title: "Vegetarische Deutsche Rezepte",
+  tagline: "Entdecke die Vielfalt der vegetarischen Küche!",
+  description: "Dein ultimativer Guide für köstliche, pflanzliche Rezepte! Entdecke eine Welt voller Geschmack und Vielfalt mit unseren einfachen und leckeren vegetarischen Gerichten.",
+  keywords: "Vegetarische Hausmannskost Rezepte,Omas fleischlose Rezepte,Vegetarische Rezepte,Vegetarische Klassiker,Vegetarische Hausmannskost Kochbuch,Typisch deutsche Gerichte vegetarisch,Landfrauen Rezepte vegetarisch,Vegetarische besondere Rezepte",
+  author: "Milyader Muda",
+  favicon: "",
   imageCount: 20,
   removeBadWords: "porn,nude,sex",
 };
